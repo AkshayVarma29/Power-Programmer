5524ce3505af110283ff1b652c0ea35f9e46ccb2
Akshay Varma <20849192+AkshayVarma29@users.noreply.github.com>
2021-08-26 14:28:14 +0530

    Added two ways of solving the sum of digits problems. One using string and reduce function. Another using while loop on the integer itself.

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -1,8 +1,22 @@
 # Given a positive integer 'num', return sum of its digits. 
 # Bonus - Can you do it without using strings?
 
+from functools import reduce
 num = 123
+# def sum_of_digits(num):
+#     num_list = list(str(num))
+#     final = reduce(lambda a,b: int(a)+int(b), num_list)
+#     return final
+
+# [Done] exited with code=0 in 0.123 seconds
+
 def sum_of_digits(num):
-    pass
+    sum_num = 0
+    while (num!=0):
+        sum_num += num % 10
+        num = num // 10
+    return sum_num
+
+# [Done] exited with code=0 in 0.118 seconds
 
 print(sum_of_digits(num))
